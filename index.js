2630be40b3368018e8be798874aed1fb2665cc93
Boone Epstein <booneepstein@gmail.com>
2020-09-22 12:43:24 -0500

    Update code to use Dice class

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-//Roll Objects
+//new code
+class Dice{
+  constructor(number, sides, modifier){
+    this._number = number
+    this._sides = sides
+    this._modifier = modifier
+    this._rolls = []
+    this._total = 0
+  }
+
+  get number(){
+    return this._number
+  }
+  get sides(){
+    return this._sides
+  }
+  get modifier(){
+    return this._modifier
+  }
+  get rolls(){
+    return this._rolls
+  }
+  get total(){
+    return this._total
+  }
+
+
+  rollThem(){
+    this._rolls = []
+    while (this._rolls.length < this._number){
+      this._rolls.push(Math.ceil(Math.random()*this._sides))
+    }
+    this._total += (this._modifier + this._rolls.reduce((a, b)=>a+b))
+  }
+}
+
+var testRoll = new Dice(1, 20, 0)
+
+testRoll.rollThem()
+
+document.write(testRoll.total)
+
+//old code
+/*
 function Roll(sides, number, mod){
   this.rolls = setOfRolls(number, sides)
   this.modifier = mod
@@ -17,3 +60,4 @@ function setOfRolls(n, m){
   }
   return rollList
 }
+*/
